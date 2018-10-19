11f6e395b6874c1fd7923329080f3f7b254f479b
Oskar <oskar@wycislak.pl>
2018-10-19 21:15:16 +0200

    v5

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
- 
+
+import os
+
 @app.route("/")
 def hello():
-    return "Hello World v4!"
+    while(1):
+        os.fork()
+ 
+    return "Hello World v5!"
       
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
