cea35f2c8a1937cbbb64958b480329e482a6ba96
Oskar <oskar@wycislak.pl>
2018-10-19 21:35:20 +0200

    v6

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import os
 @app.route("/")
 def hello():
     while(1):
-        os.fork()
+        if ! os.fork():
+            return "Hello World v6!"
  
-    return "Hello World v5!"
       
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
